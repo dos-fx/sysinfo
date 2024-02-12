38204facc4129b559690736b806a1ca6e0f82786
Layne <neonisdaboi@outlook.com>
2024-02-12 05:21:15 -0600

    Update batfetch.ps1

--- batfetch.ps1
+++ batfetch.ps1
@@ -1,4 +1,7 @@
 echo placeholder
 $url = "https://raw.githubusercontent.com/PixelNetNeon/batFetch/main/batfetch.bat"
+$env:PATH += "C:\batFetch"
+
 
 Invoke-WebRequest $url -OutFile c:\batfetch\batfetch.bat
+[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\batchFetch", "User")
