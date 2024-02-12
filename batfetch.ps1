382cc83958da1981c84fa7e2b36735e4026df43c
Layne <neonisdaboi@outlook.com>
2024-02-12 04:52:13 -0600

    Update batfetch.ps1

--- batfetch.ps1
+++ batfetch.ps1
@@ -1,4 +1,4 @@
 echo placeholder
-$url = https://raw.githubusercontent.com/PixelNetNeon/batFetch/main/batfetch.bat
+$url = "https://raw.githubusercontent.com/PixelNetNeon/batFetch/main/batfetch.bat"
 
 Invoke-WebRequest $url -OutFile c:\batfetch\batfetch.bat
