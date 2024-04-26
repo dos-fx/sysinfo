12fa3623019634d21a2c5b4145f9cc282f0d6cb8
Maple Kiara <167991948+mapliee@users.noreply.github.com>
2024-04-26 12:35:19 +0200

    Remove batfetch PowerShell script
    DOS doesn't have PowerShell, dummy!

--- batfetch.ps1
+++
@@ -1,6 +0,0 @@
-$url = "https://raw.githubusercontent.com/PixelNetNeon/batFetch/main/batfetch.bat"
-$env:PATH += "C:\batFetch"
-
-New-Item -ItemType Directory -Force -Path 'C:\batFetch'
-Invoke-WebRequest $url -OutFile c:\batFetch\batfetch.bat
-[Environment]::SetEnvironmentVariable("Path", $env:Path + ";C:\batchFetch", "User")
